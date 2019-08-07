04a8db589e85362f95b581230d3f88901849cccc
Justin Trombley <jtrombley96@gmail.com>
2019-08-06 23:06:24 -0400

    Bug fix

--- routes/postsRouter.js
+++ routes/postsRouter.js
@@ -15,36 +15,33 @@ router.get('/', (req, res) => {
 //GET not throwing back ID doesn't exist error.
 router.get('/:id', (req, res) => {
 	const postId = req.params.id;
+	if (!postId) {
+		res.status(404).json({ message: 'The user with the specified ID does not exist.' });
+	}
 	Posts.findById(postId)
-		.then(post => {
-			if (post) {
-				res.status(201).json(post);
-			} else {
-				res.status(404).json({ message: 'The post with the specified ID does not exist.' });
-			}
+		.then(postId => {
+			res.status(200).json(postId);
 		})
-		.catch(err => {
-			res.status(500).json({
-				error : 'There was an error while saving the post to the database',
-			});
+		.catch(error => {
+			res.status(500).json({ error: 'The user information could not be retrieved.' });
 		});
 });
 //GET not throwing back ID doesn't exist error.
 router.get('/:id/comments', (req, res) => {
 	const postId = req.params.id;
-	Posts.findPostComments(postId)
-		.then(post => {
-			if (post) {
-				res.status(201).json(post);
-			} else {
-				res.status(404).json({ message: 'The post with the specified ID does not exist.' });
-			}
-		})
-		.catch(err => {
-			res.status(500).json({
-				error : 'There was an error while saving the post to the database',
+	if (postId) {
+		Posts.findPostComments(postId)
+			.then(postId => {
+				res.status(201).json(postId);
+			})
+			.catch(err => {
+				res.status(500).json({
+					error : 'There was an error while saving the post to the database',
+				});
 			});
-		});
+	} else {
+		res.status(404).json({ message: 'The post with the specified ID does not exist.' });
+	}
 });
 //100% Functional
 router.post('/', (req, res) => {
