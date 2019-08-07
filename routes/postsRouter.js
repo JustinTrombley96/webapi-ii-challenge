d68ac719b369afb4f06bfcd643865293c83cd06f
Justin Trombley <jtrombley96@gmail.com>
2019-08-06 23:17:46 -0400

    Messages Fixed

--- routes/postsRouter.js
+++ routes/postsRouter.js
@@ -16,32 +16,31 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
 	const postId = req.params.id;
 	if (!postId) {
-		res.status(404).json({ message: 'The user with the specified ID does not exist.' });
+		res.status(404).json({ message: 'The post with the specified ID does not exist.' });
 	}
 	Posts.findById(postId)
 		.then(postId => {
 			res.status(200).json(postId);
 		})
 		.catch(error => {
-			res.status(500).json({ error: 'The user information could not be retrieved.' });
+			res.status(500).json({ error: 'The post information could not be retrieved.' });
 		});
 });
 //GET not throwing back ID doesn't exist error.
 router.get('/:id/comments', (req, res) => {
 	const postId = req.params.id;
 	if (postId) {
-		Posts.findPostComments(postId)
-			.then(postId => {
-				res.status(201).json(postId);
-			})
-			.catch(err => {
-				res.status(500).json({
-					error : 'There was an error while saving the post to the database',
-				});
-			});
-	} else {
 		res.status(404).json({ message: 'The post with the specified ID does not exist.' });
 	}
+	Posts.findPostComments(postId)
+		.then(postId => {
+			res.status(201).json(postId);
+		})
+		.catch(err => {
+			res.status(500).json({
+				error : 'The comments information could not be retrieved.',
+			});
+		});
 });
 //100% Functional
 router.post('/', (req, res) => {
@@ -69,7 +68,11 @@ router.post('/:id/comments', (req, res) => {
 	}
 	Posts.insertComment(comment)
 		.then(comm => {
-			res.status(201).json({ comm });
+			if (comm) {
+				res.status(201).json({ comm });
+			} else {
+				res.status(404).json({ message: 'The post with the specified ID does not exist.' });
+			}
 		})
 		.catch(err => {
 			res.status(500).json({
@@ -110,7 +113,7 @@ router.delete('/:id', (req, res) => {
 		})
 		.catch(err => {
 			res.status(500).json({
-				message : 'The post with the specified ID does not exist.',
+				error : 'The post could not be removed',
 			});
 		});
 });
